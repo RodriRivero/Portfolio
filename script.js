71ee52a29b8b8816d8cf62d7bf5464e1807174a7
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-03-27 22:29:15 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+/*responsive funcion*/
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className="responsive";
+    }else{
+        x.className= "";
+    }
+}
