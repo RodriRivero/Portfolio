00eb7c2231effd96a1ef1d7e692445d5f6e6d24f
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-03-28 22:36:59 -0300

    Portfolio

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function seleccionar(link) {
     opciones[4].className = "";
     link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    // desaparecer el menu una vez elegido
     //en modo responsive
     var x = document.getElementById("nav");
     x.className = "";
 }
 
-//función que muestra el menu responsive
+// muestra el menu responsive
 function responsiveMenu() {
     var x = document.getElementById("nav");
     if (x.className === "") {
@@ -24,18 +24,18 @@ function responsiveMenu() {
     }
 }
 
-//detecto el scrolling para aplicar la animación del la barra de habilidades
+//detecto el scrolling habilidades
 window.onscroll = function() { efectoHabilidades() };
 
-//funcion que aplica la animación de la barra de habilidades
+// aplica la animación de la barra de habilidades
 function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+        document.getElementById("ps").classList.add("barra-progreso1");
+        document.getElementById("html").classList.add("barra-progreso2");
+        document.getElementById("js").classList.add("barra-progreso3");
+        document.getElementById("gh").classList.add("barra-progreso4");
     }
 
 }
